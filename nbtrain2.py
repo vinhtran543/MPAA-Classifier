924a8735d0dc5815dbc870338f8c3d44f09e19c0
Vinh Tran <vnntran@ucdavis.edu>
2018-03-14 07:02:08 -0700

    naive bayes trainer + movietraining added
    naive bayes trainer  creates movietraining.nb, use this.
    Not sure if he wants us to automate the naivesbayes, but doesn't matter
    since all we need is the movietraining.nb

---
+++ nbtrain2.py
@@ -0,0 +1,43 @@
+import nltk
+import pickle
+import sys
+import os
+
+# Checks commandline arguments are atleast 3
+if(len(sys.argv) < 3):
+    print("Error: Insufficient commandline arguments!")
+    exit()
+
+directory = sys.argv[1]
+
+# Get Class Types
+classes = []
+for f in os.listdir(directory):
+    temp = f.split('-')
+    #print(temp)
+    if(temp[1] == '13' or temp[1] == '17'):
+        temp2 = temp[0] + '-' + temp[1]
+        if not(temp2 in classes):
+            classes.append(temp2)
+            
+    if not(temp[0] in classes):
+        classes.append(temp[0])
+
+print(classes)
+
+model = []
+# Extract Corpuses
+for c in classes:
+    c_corpus = nltk.corpus.PlaintextCorpusReader(directory, c + '.*.txt')
+    print(c_corpus)
+    c_fd = nltk.FreqDist(c_corpus.words())
+    c_count = len(c_corpus.words())
+    model.append({'label':c, 'count':c_count, 'fd':c_fd})
+
+pickle.dump(model, open(sys.argv[2], 'wb'))
+
+print('Training Data Created:')
+print('\tClasses: {0}'.format(classes))
+print('\tStored: {0}'.format(sys.argv[2]))
+
+
